3185979446704e0e2344844b9065508a7e95cf93
henryagyare <83439356+henryagyare@users.noreply.github.com>
2024-07-02 22:56:39 +0000

    Initial commit

---
+++ desEncryt.srcs/sources_1/new/reg32.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/02/2024 10:04:01 PM
+// Design Name: 
+// Module Name: reg32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reg32(
+    input logic clk, reg32Load,
+    input logic [31:0] reg32Input,
+    output logic [31:0] reg32Output
+    );
+    
+    
+    always_ff @(posedge clk)
+    begin
+        if (reg32Load == 1'b1)      
+            reg32Output <= reg32Input;
+    end    
+endmodule
---
+++ desEncryt.srcs/sources_1/new/reg48.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/02/2024 10:34:27 PM
+// Design Name: 
+// Module Name: reg48
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reg48(
+
+    );
+endmodule
